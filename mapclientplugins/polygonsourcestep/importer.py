8440a9917d81cc3ca06c212f9b9d013a38203517
TimmyNic <tim.nicolas@outlook.com>
2021-06-08 10:47:21 +1200

    Fixed syntax error.

--- mapclientplugins/polygonsourcestep/importer.py
+++ mapclientplugins/polygonsourcestep/importer.py
@@ -72,7 +72,7 @@ class Reader(object):
         self.polydata = actors.GetNextActor().GetMapper().GetInput()
 
         if self.polydata.GetPoints() == None:
-            raise IOError, 'file not loaded'
+            raise IOError('file not loaded')
         else:
             self._loadPoints()
             self._loadTriangles()
@@ -87,7 +87,7 @@ class Reader(object):
         self.polydata = r.GetOutput()
 
         if self.polydata.GetPoints() == None:
-            raise IOError, 'file not loaded'
+            raise IOError('file not loaded')
         else:
             self._loadPoints()
             self._loadTriangles()
@@ -102,7 +102,7 @@ class Reader(object):
         self.polydata = r.GetOutput()
 
         if self.polydata.GetPoints() == None:
-            raise IOError, 'file not loaded'
+            raise IOError('file not loaded')
         else:
             self._loadPoints()
             self._loadTriangles()
@@ -117,7 +117,7 @@ class Reader(object):
         self.polydata = r.GetOutput()
 
         if self.polydata.GetPoints() == None:
-            raise IOError, 'file not loaded'
+            raise IOError('file not loaded')
         else:
             self._loadPoints()
             self._loadTriangles()
@@ -135,7 +135,7 @@ class Reader(object):
         self.polydata = r.GetOutput()
 
         if self.polydata.GetPoints() == None:
-            raise IOError, 'file not loaded'
+            raise IOError('file not loaded')
         else:
             self._loadPoints()
             self._loadTriangles()
